4b2cb36b154afdbb6cfda1d70c9c657e8fc14b03
Gustavo Maretti <gustavohmaretti@outlook.com>
2024-03-15 14:27:11 -0300

    Base do projeto controle de estoque com as funções de adição e subtração de itens

---
+++ ControleDeEstoquePOO/Estoque.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ControleDeEstoquePOO
+{
+    class Estoque
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+             Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        { 
+            Quantidade -= quantidade;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", valor unitário: R$ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", quantidade em estoque: "
+                + Quantidade
+                + " unidades, valor total em estoque: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);  
+        }
+    }
+}
---
+++ ControleDeEstoquePOO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleDeEstoquePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estoque estoque = new Estoque();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            estoque.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            estoque.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            estoque.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + estoque);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            estoque.AdicionarProdutos(qtd);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + estoque);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            estoque.RemoverProdutos(qtd);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + estoque);
+
+
+
+
+
+        }
+    }
+}
